b11f6cb130e500934004c38a80453c384694e048
liudengdeng <1034316694@qq.com>
2018-07-10 20:35:21 +0800

    helloworld

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+package java;
+
+/**
+ * 类名 HelloWorld
+ * 描述 helloworld
+ * 作者 Administrator
+ * 时间 2018/7/10 20:11
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("helloWorld!!");
+    }
+}
